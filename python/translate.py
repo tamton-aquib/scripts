23cbe4786566bc514ff56aba38b5b6affd3b28ed
tamton-aquib <aquibjavedt007@gmail.com>
2021-10-20 19:11:47 +0530

    adding default geometry to translate.py

--- python/translate.py
+++ python/translate.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 import googletrans
-from tkinter import Tk, Label
+from tkinter import Tk, Label, Button
 
 root = Tk()
+root.geometry('640x400+320+180')
 
 sentence = root.clipboard_get()
 
 translator = googletrans.Translator()
 nice = translator.translate(sentence)
 
-label = Label(root, text=nice.text)
-label.pack()
+for i in range(5):
+	Label(root, text=f" ").pack()
+	
+Label(root, text=f"From: {nice.src}").pack()
+Label(root, text=f"To  : {nice.dest}").pack()
+Label(root, text=f"Translation: {nice.text}").pack()
+
+close_button = Button(root, text="close", command=root.destroy).pack()
 
 root.mainloop()
