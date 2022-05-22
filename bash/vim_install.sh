83a27dfe94c07645e1d6d693577bea9df869ad77
tamton-aquib <aquibjavedt007@gmail.com>
2022-05-22 15:05:37 +0530

    updating vim_install script

--- bash/vim_install.sh
+++ bash/vim_install.sh
@@ -4,28 +4,17 @@ R="\033[91m"
 G="\033[92m"
 E="\033[0m"
 
-read -p "Which vim are you using? [vim/nvim]: " VIM_VERSION
-
-#### VIM ####
-if [[ "$VIM_VERSION" == "vim" ]]; then
-	curl -s -Lo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-	curl -s "https://raw.githubusercontent.com/tamton-aquib/dotfiles/main/.vimrc" > ~/.vimrc
-	vim -c ":PlugInstall"
-	echo "colorscheme onedark" >> ~/.vimrc
-
 #### NVIM ####
-elif [[ "$VIM_VERSION" == "nvim" ]]; then
-	read -p "VimL config or lua [viml/lua]: " VIM_OR_LUA
-	if [[ "$VIM_OR_LUA" == "vim" ]]; then
-		curl -s -Lo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-		mkdir -p ~/.config/nvim
-		curl -s "https://raw.githubusercontent.com/tamton-aquib/dotfiles/main/.vimrc" > ~/.config/nvim/init.vim
-		nvim -c "echo 'Installing Plugins (Usually takes around 30s.)' | :PlugInstall"
-		echo "colorscheme onedark" >> ~/.config/nvim/init.vim
-	else
-		curl -sL git.io/tajvim | bash
-	fi
-
-else
-	echo -e "${R}Enter valid vim version.${E}"
+if [[ ! -d "~/.local/share/nvim/site/pack/packer" ]]; then
+    echo "Deleting old packer directory..."
+    rm -rf ~/.local/share/nvim/site/pack/packer
 fi
+
+printf "${G}Installing packer...${E}\n"
+sleep 1
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim 2>/dev/null
+mkdir -p ~/.config/nvim
+printf "${G}Cloning repo...${E}\n"
+sleep 2
+git clone --depth 1 https://github.com/tamton-aquib/nvim.git ~/.config/nvim 2>/dev/null
+nvim +PackerSync
---
+++ bash/vim_install_old.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+R="\033[91m"
+G="\033[92m"
+E="\033[0m"
+
+read -p "Which vim are you using? [vim/nvim]: " VIM_VERSION
+
+#### VIM ####
+if [[ "$VIM_VERSION" == "vim" ]]; then
+	curl -s -Lo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	curl -s "https://raw.githubusercontent.com/tamton-aquib/dotfiles/main/.vimrc" > ~/.vimrc
+	vim -c ":PlugInstall"
+	echo "colorscheme onedark" >> ~/.vimrc
+
+#### NVIM ####
+elif [[ "$VIM_VERSION" == "nvim" ]]; then
+	read -p "VimL config or lua [viml/lua]: " VIM_OR_LUA
+	if [[ "$VIM_OR_LUA" == "vim" ]]; then
+		curl -s -Lo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+		mkdir -p ~/.config/nvim
+		curl -s "https://raw.githubusercontent.com/tamton-aquib/dotfiles/main/.vimrc" > ~/.config/nvim/init.vim
+		nvim -c "echo 'Installing Plugins (Usually takes around 30s.)' | :PlugInstall"
+		echo "colorscheme onedark" >> ~/.config/nvim/init.vim
+	else
+		curl -sL git.io/tajvim | bash
+	fi
+else
+	echo -e "${R}Enter valid vim version.${E}"
+fi
